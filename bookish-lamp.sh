bd1e74dd6e321975a45a8d2f1b567f02aa186326
Attila Pinter <apinter.it@gmail.com>
2015-12-14 22:45:01 +0700

    Small modification and beutification on the code

--- bookish-lamp.sh
+++ bookish-lamp.sh
@@ -5,13 +5,15 @@ DEST=Arch
 DISK=AuroraTerra
 
 if mountpoint -q /run/media/$HOME_USER/AuroraTera/
-then
-echo "filesystem mounted"
-rsync -av /home/$HOME_USER /run/media/$HOME_USER/$DISK/Backup/$DEST/home/$HOME_USER
-rsync -av /var /run/media/$HOME_USER/$DISK/Backup/$DEST/var
-rsync -av /etc /run/media/$HOME_USER/$DISK/Backup/$DEST/etc
+	then
+	echo "filesystem mounted >> STARTING BACKUP"
+		rsync -av /home/$HOME_USER /run/media/$HOME_USER/$DISK/Backup/$DEST/home/$HOME_USER
+		rsync -av /var /run/media/$HOME_USER/$DISK/Backup/$DEST/var
+		rsync -av /etc /run/media/$HOME_USER/$DISK/Backup/$DEST/etc
 
-
-else
-echo "filesystem not mounted"
-fi
+	echo "Backup is finished"
+##OPTIONAL - modify sudoers to don't ask password for shutdown
+##sudo shutdown -h
+	else
+		echo "filesystem not mounted"
+	fi
