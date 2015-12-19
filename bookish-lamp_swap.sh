136a1bc5c82da171c44089caba0571dfda4a8a87
Attila Pinter <apinter.it@gmail.com>
2015-12-19 15:41:27 +0700

    Destination file added

--- bookish-lamp_swap.sh
+++ bookish-lamp_swap.sh
@@ -3,7 +3,7 @@
 ##The point of this script is to automize the modifications in config files.
 ##Changing swap settings is one of the most frequent change I make to a new system:
 
-ed -s /etc/ << 'EOF'
+ed -s /etc/sysctl.conf << 'EOF'
 $a
 vm.swappiness=10
 vm.vfs_cache_pressure = 50
