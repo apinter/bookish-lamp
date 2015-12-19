906c825d4d36bc939ba5722108cab8dfd9a3ba73
Attila Pinter <apinter.it@gmail.com>
2015-12-19 14:39:58 +0700

    Swappiness settings changing script

---
+++ bookish-lamp_swap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##The point of this script is to automize the modifications in config files.
+##Changing swap settings is one of the most frequent change I make to a new system:
+
+ed -s /etc/ << 'EOF'
+$a
+vm.swappiness=10
+vm.vfs_cache_pressure = 50
+.
+w
+EOF
+
